6d9faca1f2e0230d9460af5c07a3be6ce67a7339
Nikita Barinov <numer140466@list.ru>
2019-12-04 08:22:17 +0300

    new_project

---
+++ client_side.h
@@ -0,0 +1,13 @@
+#ifndef CLIENT_SIDE_H_INCLUDED
+#define CLIENT_SIDE_H_INCLUDED
+
+class client_side
+{
+public:
+client_side(){};
+~client_side(){};
+int client_main();
+};
+
+
+#endif // CLIENT_SIDE_H_INCLUDED
